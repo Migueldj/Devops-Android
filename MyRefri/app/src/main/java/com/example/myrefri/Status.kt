895b936e670378b7f3be577bc1faddf653f47798
Joaquin Dguez <joaquindominguez_g@hotmail.com>
2021-11-29 21:14:18 -0600

    Creando 3 actividades, para diferentes ventanas

--- MyRefri/app/src/main/java/com/example/myrefri/Status.kt
+++ MyRefri/app/src/main/java/com/example/myrefri/Status.kt
@@ -3,10 +3,9 @@ package com.example.myrefri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Status : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_status)
     }
 }
