54ae4990d43079a9ecf6e9b7f1b2fae3592b5d77
Migueldj <36450896+Migueldj@users.noreply.github.com>
2021-11-29 21:43:55 -0600

    Configuración de la base de datos

--- MyRefri/app/src/main/java/com/example/myrefri/MainActivity.kt
+++ MyRefri/app/src/main/java/com/example/myrefri/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myrefri
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import com.google.firebase.analytics.FirebaseAnalytics
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message","Firebase al 100")
+        analytics.logEvent("InitScreen", bundle)
     }
 }
