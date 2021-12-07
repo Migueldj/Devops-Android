a683615a5c339b408ae770641e6f0d543c85d472
Migueldj <36450896+Migueldj@users.noreply.github.com>
2021-12-06 19:01:44 -0600

    Ya funciona el login de la aplicación

--- MyRefri/app/src/main/java/com/example/myrefri/Login.kt
+++ MyRefri/app/src/main/java/com/example/myrefri/Login.kt
@@ -1,24 +1,58 @@
 package com.example.myrefri
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
+    private fun updateUI(user: FirebaseUser?) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         val button = findViewById<Button>(R.id.btn_login)
         button.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+            //val intent = Intent(this,MainActivity::class.java)
+            //startActivity(intent)
+            val editTextTextEmailAddress = findViewById<EditText>(R.id.editTextTextEmailAddress)
+            val editTextTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+            val email = editTextTextEmailAddress.text.toString()
+            val password = editTextTextPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        //updateUI(user)
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Usuario no registrado",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
         }
-
-
-
     }
 }
