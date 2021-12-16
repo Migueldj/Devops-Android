c0fad200d763e44c790590e0fde7c8dc90092175
juanjg97 <juanjosejg2711@gmail.com>
2021-12-16 13:38:45 -0600

    Se comentó el código en Status y Nivel, se modificó el nombre de algunas variables

--- MyRefri/app/src/main/java/com/example/myrefri/Nivel.kt
+++ MyRefri/app/src/main/java/com/example/myrefri/Nivel.kt
@@ -1,113 +1,116 @@
 package com.example.myrefri
-
 import android.content.Intent
 import android.widget.ImageView
 import android.widget.TextView
 
-class Nivel(var i_nivel:Int, var lista_comida:Array<String>,
-            var lista_tv:Array<TextView>, var lista_imv:Array<ImageView>) {
-
-    //Función para configurar el textView
+class Nivel(var nivel_int:Int, var productos_ar:Array<String>, var txtview_ar:Array<TextView>, var imview_ar:Array<ImageView>) {
+    //-------------------------------------------------------------------------
+    //Función para configurar los textView de un nivel
+    //Para los 3 textView del nivel elegido, asignale a cada uno, el aliemnto correspondiente
     fun config_tv(){
-        for (i in (0 until this.lista_tv.size)){
-            this.lista_tv[i].text=this.lista_comida[0]
+        for (i in (0 until this.txtview_ar.size)){
+            //Define el textView que recibió el constructor con el tipo de alimento que recibió el constructor
+            this.txtview_ar[i].text=this.productos_ar[0]
         }
     }
-
-
-    //Función para configutar el imageView
+    //-------------------------------------------------------------------------
+    //Función para configurar los imageView de un nivel
     fun config_imv(){
-        val imv_1: ImageView =  this.lista_imv[0]
-        val imv_2: ImageView =  this.lista_imv[1]
-        val imv_3: ImageView =  this.lista_imv[2]
+        //Asigna a la variable imv_ los imageView del nivel que recibió el constructor
+        val imv_1: ImageView =  this.imview_ar[0]
+        val imv_2: ImageView =  this.imview_ar[1]
+        val imv_3: ImageView =  this.imview_ar[2]
 
-        if(this.i_nivel==1){
-            when(this.lista_comida[0]){
+        //Si el constructor recibió el nivel 1
+        if(this.nivel_int==1){
+            //Cuando el producto sea "..." establece el imv_1 del nivel 1 como "..."
+            when(this.productos_ar[0]){
                 "Res"    ->  imv_1.setImageResource(R.drawable.res)  ;"Puerco" ->  imv_1.setImageResource(R.drawable.puerco)
                 "Pollo"  ->  imv_1.setImageResource(R.drawable.pollo);"Pescado" -> imv_1.setImageResource(R.drawable.pescado)
             }
-
-            when(this.lista_comida[1]){
+            //Cuando el producto sea "..." establece el imv_2 del nivel 1 como "..."
+            when(this.productos_ar[1]){
                 "Res"    ->  imv_2.setImageResource(R.drawable.res)  ;"Puerco"  ->  imv_2.setImageResource(R.drawable.puerco)
                 "Pollo"  ->  imv_2.setImageResource(R.drawable.pollo);"Pescado" ->  imv_2.setImageResource(R.drawable.pescado)
             }
-            when(this.lista_comida[2]){
+            when(this.productos_ar[2]){
                 "Res"    ->  imv_3.setImageResource(R.drawable.res)  ;"Puerco" ->  imv_3.setImageResource(R.drawable.puerco)
                 "Pollo"  ->  imv_3.setImageResource(R.drawable.pollo);"Pescado" -> imv_3.setImageResource(R.drawable.pescado)
             }
         }
-        if(this.i_nivel==2){
-            when(this.lista_comida[0]){
+        //Si el constructor recibió el nivel 2
+        if(this.nivel_int==2){
+            //Cuando el producto sea "..." establece el imv_1 del nivel 2 como "..."
+            when(this.productos_ar[0]){
                 "Paletas"    ->  imv_1.setImageResource(R.drawable.paleta)
                 "Helado"     ->  imv_1.setImageResource(R.drawable.helado)
                 "Chocolate"  ->  imv_1.setImageResource(R.drawable.chocolate)
             }
-            when(this.lista_comida[1]){
+            //Cuando el producto sea "..." establece el imv_2 del nivel 2 como "..."
+            when(this.productos_ar[1]){
                 "Paletas"    ->  imv_2.setImageResource(R.drawable.paleta)
                 "Helado"     ->  imv_2.setImageResource(R.drawable.helado)
                 "Chocolate"  ->  imv_2.setImageResource(R.drawable.chocolate)
             }
-            when(this.lista_comida[2]){
+            when(this.productos_ar[2]){
                 "Paletas"    ->  imv_3.setImageResource(R.drawable.paleta)
                 "Helado"     ->  imv_3.setImageResource(R.drawable.helado)
                 "Chocolate"  ->  imv_3.setImageResource(R.drawable.chocolate)
             }
         }
-        if(this.i_nivel==3){
-            when(this.lista_comida[0]){
+        if(this.nivel_int==3){
+            when(this.productos_ar[0]){
                 "Quesos"    ->  imv_1.setImageResource(R.drawable.queso)
                 "Jamon"     ->  imv_1.setImageResource(R.drawable.jamon)
                 "Salchicha" ->  imv_1.setImageResource(R.drawable.salchicha)
             }
-            when(this.lista_comida[1]){
+            when(this.productos_ar[1]){
                 "Quesos"    ->  imv_2.setImageResource(R.drawable.queso)
                 "Jamon"     ->  imv_2.setImageResource(R.drawable.jamon)
                 "Salchicha" ->  imv_2.setImageResource(R.drawable.salchicha)
             }
-            when(this.lista_comida[2]){
+            when(this.productos_ar[2]){
                 "Quesos"    ->  imv_3.setImageResource(R.drawable.queso)
                 "Jamon"     ->  imv_3.setImageResource(R.drawable.jamon)
                 "Salchicha" ->  imv_3.setImageResource(R.drawable.salchicha)
             }
         }
-        if(this.i_nivel==4){
-            when(this.lista_comida[0]){
+        if(this.nivel_int==4){
+            when(this.productos_ar[0]){
                 "Yogurt"   ->  imv_1.setImageResource(R.drawable.yogurt)
                 "Leche"    ->  imv_1.setImageResource(R.drawable.leche)
                 "Gelatina" ->  imv_1.setImageResource(R.drawable.gelatina)
                 "Huevo"    ->  imv_1.setImageResource(R.drawable.huevos)
             }
-            when(this.lista_comida[1]){
+            when(this.productos_ar[1]){
                 "Yogurt"   ->  imv_2.setImageResource(R.drawable.yogurt)
                 "Leche"    ->  imv_2.setImageResource(R.drawable.leche)
                 "Gelatina" ->  imv_2.setImageResource(R.drawable.gelatina)
                 "Huevo"    ->  imv_2.setImageResource(R.drawable.huevos)
             }
-            when(this.lista_comida[2]){
+            when(this.productos_ar[2]){
                 "Yogurt"   ->  imv_3.setImageResource(R.drawable.yogurt)
                 "Leche"    ->  imv_3.setImageResource(R.drawable.leche)
                 "Gelatina" ->  imv_3.setImageResource(R.drawable.gelatina)
                 "Huevo"    ->  imv_3.setImageResource(R.drawable.huevos)
             }
-
         }
-        if(this.i_nivel==5){
-            when(this.lista_comida[0]){
+        if(this.nivel_int==5){
+            when(this.productos_ar[0]){
                 "Comida Sobrante" -> imv_1.setImageResource(R.drawable.comidasobrante)
                 "Agua"            -> imv_1.setImageResource(R.drawable.agua)
             }
-            when(this.lista_comida[1]){
+            when(this.productos_ar[1]){
                 "Comida Sobrante" -> imv_2.setImageResource(R.drawable.comidasobrante)
                 "Agua"            -> imv_2.setImageResource(R.drawable.agua)
             }
-            when(this.lista_comida[2]){
+            when(this.productos_ar[2]){
                 "Comida Sobrante" -> imv_3.setImageResource(R.drawable.comidasobrante)
                 "Agua"            -> imv_3.setImageResource(R.drawable.agua)
             }
-
         }
-        if(this.i_nivel==6){
-            when(this.lista_comida[0]){
+        if(this.nivel_int==6){
+            when(this.productos_ar[0]){
                 "Jitomate" ->  imv_1.setImageResource(R.drawable.jitomate)
                 "Cebolla"  ->  imv_1.setImageResource(R.drawable.cebolla)
                 "Chiles"   ->  imv_1.setImageResource(R.drawable.chiles)
@@ -115,7 +118,7 @@ class Nivel(var i_nivel:Int, var lista_comida:Array<String>,
                 "Verduras" ->  imv_1.setImageResource(R.drawable.verduras)
                 "Otros"    ->  imv_1.setImageResource(R.drawable.verduras)
             }
-            when(this.lista_comida[1]){
+            when(this.productos_ar[1]){
                 "Jitomate" ->  imv_2.setImageResource(R.drawable.jitomate)
                 "Cebolla"  ->  imv_2.setImageResource(R.drawable.cebolla)
                 "Chiles"   ->  imv_2.setImageResource(R.drawable.chiles)
@@ -123,7 +126,7 @@ class Nivel(var i_nivel:Int, var lista_comida:Array<String>,
                 "Verduras" ->  imv_2.setImageResource(R.drawable.verduras)
                 "Otros"    ->  imv_2.setImageResource(R.drawable.verduras)
             }
-            when(this.lista_comida[2]){
+            when(this.productos_ar[2]){
                 "Jitomate" ->  imv_3.setImageResource(R.drawable.jitomate)
                 "Cebolla"  ->  imv_3.setImageResource(R.drawable.cebolla)
                 "Chiles"   ->  imv_3.setImageResource(R.drawable.chiles)
@@ -133,6 +136,4 @@ class Nivel(var i_nivel:Int, var lista_comida:Array<String>,
             }
         }
     }
-
-
 }
--- MyRefri/app/src/main/java/com/example/myrefri/Status.kt
+++ MyRefri/app/src/main/java/com/example/myrefri/Status.kt
@@ -24,58 +24,52 @@ class Status : AppCompatActivity() {
         database = Firebase.database.reference
         readData("Res")
 
-        //Vectores con la información
-        var niveles :Array<String> = arrayOf("Nivel1","Nivel2","Nivel3","Nivel4","Nivel5","Nivel6")
+        //Arreglo con los nombres de los niveles establecidos en la actividad Main
+        var niveles_ar :Array<String> = arrayOf("Nivel1","Nivel2","Nivel3","Nivel4","Nivel5","Nivel6")
 
-        var comidas = arrayOf(
-            arrayOf("comida_n1_1","comida_n1_2","comida_n1_3") ,
-            arrayOf("comida_n2_1","comida_n2_2","comida_n2_3") ,
-            arrayOf("comida_n3_1","comida_n3_2","comida_n3_3") ,
-            arrayOf("comida_n4_1","comida_n4_2","comida_n4_3") ,
-            arrayOf("comida_n5_1","comida_n5_2","comida_n5_3") ,
-            arrayOf("comida_n6_1","comida_n6_2","comida_n6_3") ,
+        //Matriz para posteriormente almacenar la información de la actividad Main
+        var productos_mat = arrayOf(
+            arrayOf("producto_n1_1","producto_n1_2","producto_n1_3") ,
+            arrayOf("producto_n2_1","producto_n2_2","producto_n2_3") ,
+            arrayOf("producto_n3_1","producto_n3_2","producto_n3_3") ,
+            arrayOf("producto_n4_1","producto_n4_2","producto_n4_3") ,
+            arrayOf("producto_n5_1","producto_n5_2","producto_n5_3") ,
+            arrayOf("producto_n6_1","producto_n6_2","producto_n6_3") ,
         )
 
-        var tview_n1:Array<TextView>   = arrayOf(findViewById(R.id.tv_comida_n1_1),findViewById(R.id.tv_comida_n1_2),findViewById(R.id.tv_comida_n1_3))
-        var tview_n2:Array<TextView>   = arrayOf(findViewById(R.id.tv_comida_n2_1),findViewById(R.id.tv_comida_n2_2),findViewById(R.id.tv_comida_n2_3))
-        var tview_n3:Array<TextView>   = arrayOf(findViewById(R.id.tv_comida_n3_1),findViewById(R.id.tv_comida_n3_2),findViewById(R.id.tv_comida_n3_3))
-        var tview_n4:Array<TextView>   = arrayOf(findViewById(R.id.tv_comida_n4_1),findViewById(R.id.tv_comida_n4_2),findViewById(R.id.tv_comida_n4_3))
-        var tview_n5:Array<TextView>   = arrayOf(findViewById(R.id.tv_comida_n5_1),findViewById(R.id.tv_comida_n5_2),findViewById(R.id.tv_comida_n5_3))
-        var tview_n6:Array<TextView>   = arrayOf(findViewById(R.id.tv_comida_n6_1),findViewById(R.id.tv_comida_n6_2),findViewById(R.id.tv_comida_n6_3))
-
-        var imview_n1:Array<ImageView> = arrayOf(findViewById(R.id.iv_n1_1),findViewById(R.id.iv_n1_2),findViewById(R.id.iv_n1_3))
-        var imview_n2:Array<ImageView> = arrayOf(findViewById(R.id.iv_n2_1),findViewById(R.id.iv_n2_2),findViewById(R.id.iv_n2_3))
-        var imview_n3:Array<ImageView> = arrayOf(findViewById(R.id.iv_n3_1),findViewById(R.id.iv_n3_2),findViewById(R.id.iv_n3_3))
-        var imview_n4:Array<ImageView> = arrayOf(findViewById(R.id.iv_n4_1),findViewById(R.id.iv_n4_2),findViewById(R.id.iv_n4_3))
-        var imview_n5:Array<ImageView> = arrayOf(findViewById(R.id.iv_n5_1),findViewById(R.id.iv_n5_2),findViewById(R.id.iv_n5_3))
-        var imview_n6:Array<ImageView> = arrayOf(findViewById(R.id.iv_n6_1),findViewById(R.id.iv_n6_2),findViewById(R.id.iv_n6_3))
-
-
-        for (i in (0 until comidas.size)){
-            for(j in (0 until comidas[i].size)){
-                comidas[i][j]=intent.getStringExtra(niveles[i]).toString()
+        //Ciclo para obtener el nombre producto de la actividad Main y asignarlo a el producto correspondiente en la matriz productos_main
+        for (i in (0 until productos_mat.size)){
+            for(j in (0 until productos_mat[i].size)){
+                productos_mat[i][j]=intent.getStringExtra(niveles_ar[i]).toString()
             }
         }
 
+        //Arreglos que contienen los id para establecer el nombre del producto y su imagen
+        var txtview_ar_n1:Array<TextView>  = arrayOf(findViewById(R.id.tv_producto_n1_1),findViewById(R.id.tv_producto_n1_2),findViewById(R.id.tv_producto_n1_3))
+        var txtview_ar_n2:Array<TextView>  = arrayOf(findViewById(R.id.tv_producto_n2_1),findViewById(R.id.tv_producto_n2_2),findViewById(R.id.tv_producto_n2_3))
+        var txtview_ar_n3:Array<TextView>  = arrayOf(findViewById(R.id.tv_producto_n3_1),findViewById(R.id.tv_producto_n3_2),findViewById(R.id.tv_producto_n3_3))
+        var txtview_ar_n4:Array<TextView>  = arrayOf(findViewById(R.id.tv_producto_n4_1),findViewById(R.id.tv_producto_n4_2),findViewById(R.id.tv_producto_n4_3))
+        var txtview_ar_n5:Array<TextView>  = arrayOf(findViewById(R.id.tv_producto_n5_1),findViewById(R.id.tv_producto_n5_2),findViewById(R.id.tv_producto_n5_3))
+        var txtview_ar_n6:Array<TextView>  = arrayOf(findViewById(R.id.tv_producto_n6_1),findViewById(R.id.tv_producto_n6_2),findViewById(R.id.tv_producto_n6_3))
+
+        var imview_ar_n1:Array<ImageView> = arrayOf(findViewById(R.id.iv_n1_1),findViewById(R.id.iv_n1_2),findViewById(R.id.iv_n1_3))
+        var imview_ar_n2:Array<ImageView> = arrayOf(findViewById(R.id.iv_n2_1),findViewById(R.id.iv_n2_2),findViewById(R.id.iv_n2_3))
+        var imview_ar_n3:Array<ImageView> = arrayOf(findViewById(R.id.iv_n3_1),findViewById(R.id.iv_n3_2),findViewById(R.id.iv_n3_3))
+        var imview_ar_n4:Array<ImageView> = arrayOf(findViewById(R.id.iv_n4_1),findViewById(R.id.iv_n4_2),findViewById(R.id.iv_n4_3))
+        var imview_ar_n5:Array<ImageView> = arrayOf(findViewById(R.id.iv_n5_1),findViewById(R.id.iv_n5_2),findViewById(R.id.iv_n5_3))
+        var imview_ar_n6:Array<ImageView> = arrayOf(findViewById(R.id.iv_n6_1),findViewById(R.id.iv_n6_2),findViewById(R.id.iv_n6_3))
 
 
         //Se crea cada nivel, usando la clase Nivel
-        val Nivel1 :Nivel = Nivel(1, comidas[0], tview_n1, imview_n1)
+        //Nivel en número entero, productos del nivel correspondiente en la matriz de productos, 3 textView correspondientes al nivel, 3 imageView correspondientes al nivel
+        val Nivel1 :Nivel = Nivel(1, productos_mat[0], txtview_ar_n1, imview_ar_n1)
+        val Nivel2 :Nivel = Nivel(2, productos_mat[1], txtview_ar_n2, imview_ar_n2)
+        val Nivel3 :Nivel = Nivel(3, productos_mat[2], txtview_ar_n3, imview_ar_n3)
+        val Nivel4 :Nivel = Nivel(4, productos_mat[3], txtview_ar_n4, imview_ar_n4)
+        val Nivel5 :Nivel = Nivel(5, productos_mat[4], txtview_ar_n5, imview_ar_n5)
+        val Nivel6 :Nivel = Nivel(6, productos_mat[5], txtview_ar_n6, imview_ar_n6)
 
-        val Nivel2 :Nivel = Nivel(2, comidas[1], tview_n2, imview_n2)
-
-        val Nivel3 :Nivel = Nivel(3, comidas[2], tview_n3, imview_n3)
-
-        val Nivel4 :Nivel = Nivel(4, comidas[3], tview_n4, imview_n4)
-
-        val Nivel5 :Nivel = Nivel(5, comidas[4], tview_n5, imview_n5)
-
-        val Nivel6 :Nivel = Nivel(6, comidas[5], tview_n6, imview_n6)
-
-
-        //Se usan las funciones config_tv para los textView y config config_imv para los ImageView
-        //Para mostrar la comida seleccionada en la siguiente interfaz
-
+        //Se usan las funciones config_tv y config_imv para mostrar el nombre e imagen de cada alimento en la interfaz Status
         Nivel1.config_tv();Nivel1.config_imv()
         Nivel2.config_tv();Nivel2.config_imv()
         Nivel3.config_tv();Nivel3.config_imv()
@@ -83,18 +77,16 @@ class Status : AppCompatActivity() {
         Nivel5.config_tv();Nivel5.config_imv()
         Nivel6.config_tv();Nivel6.config_imv()
 
-
-
-        //Código para que al pulsar el botón pasa a la ventana correspondiente
+        //Código para pasar a la actividad ShoppingList
         val button = findViewById<Button>(R.id.btn_hacer_lista)
         button.setOnClickListener{
             val intent2 = Intent(this,ShoppingList::class.java)
             startActivity(intent2)
         }
-
-
     }
-
+    
+    //---------------------------------------------
+    //Código de backend
     private  fun readData(element: String) {
         database = FirebaseDatabase.getInstance().getReference("Elements")
         database.child(element).get().addOnSuccessListener {
